43a436270acff0494ab1e03ec712c32db9ef8271
Richard St. Pierre <rst.pie@ra.rockwell.com>
2024-02-20 11:22:45 -0500

    Add way to search git with submodules

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,19 @@
 if vim.loop.os_uname().sysname == "Windows_NT" then
   return {
-  "nvim-telescope/telescope-fzf-native.nvim",
-  build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-  enabled = vim.fn.executable("cmake") == 1,
+    { 
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+      enabled = vim.fn.executable("cmake") == 1,
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      keys = {
+        { "<leader>fg", 
+          function () require("telescope.builtin").git_files({recurse_submodules = true}) end,
+          desc = "Find Git Files" 
+        }
+      }
+    }
   }
 else
   return {}
