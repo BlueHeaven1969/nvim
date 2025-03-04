525a4294cad709c26789b03c5122718d71edad74
Richard St. Pierre <rst.pie@ra.rockwell.com>
2025-03-04 11:13:55 -0500

    Set Python LSPs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -17,3 +17,9 @@ vim.g.loaded_ruby_provider = 0
 
 vim.opt.title = true
 vim.opt.titlestring = '[nvim] %t %m'
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
