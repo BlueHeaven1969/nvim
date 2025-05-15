aeacd43ed5011e28f358a1a2efd13ffabee86a04
Rick St Pierre <donator_mobile244@rick22.com>
2025-05-15 18:41:10 -0400

    Updates using luasnip

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
         -- import any extras modules here
         { import = "lazyvim.plugins.extras.coding.blink" },
+        { import = "lazyvim.plugins.extras.coding.luasnip" },
         { import = "lazyvim.plugins.extras.coding.yanky" },
         { import = "lazyvim.plugins.extras.editor.aerial" },
         { import = "lazyvim.plugins.extras.editor.navic" },
